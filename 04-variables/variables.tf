135aa86acc2fdde462f0c19c761fefa61d10d66b
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-06 16:24:43 +0530

    updating ./learn-terraform

--- 04-variables/variables.tf
+++ 04-variables/variables.tf
@@ -4,3 +4,10 @@ variable "sample" {
 output "sample1" {
   value = var.sample
 }
+
+variable "sample1" {} # here we are not giving any value , or not declaring sample 1 ,
+                      # actually it will fetch the sample 1 from terraform.tfvars file that we created
+
+output "sample1" {
+  value = var.sample1
+}
