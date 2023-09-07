5a2b0885ed755221e619b422a90d8ceb9319e81c
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-07 15:02:44 +0530

    updating ./learn-terraform

--- 04-variables/variables.tf
+++ 04-variables/variables.tf
@@ -18,4 +18,22 @@ output "cli1" {
   value = var.cli
 }
 
-variable "input" {} 
+variable "input" {} # we are not even printing this
+
+variable "type1" {
+  default = "Namaste" # this is normal one
+}
+variable "type2" {
+  default = ["Namaste",1000,true,"World"] # this is list
+}
+variable "type3" { # this is map type
+  default = {
+    string = "Hello", # here string , number and boolean are just name or key given
+    number = 100 ,
+    boolean = true
+  }
+}
+
+output "mix_type" {
+  value = "Variable Type1 is ${var.type1} , First value in list is ${var.type2[0]} , Boolean Value of Map is ${var.type3[boolean]}"
+}
