d2202b5b172da66d41b882a3ec2e114155a8b98b
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-09 17:34:04 +0530

    updating ./learn-terraform

--- roboshop-setup/example2/create_servers_loops.tf
+++ roboshop-setup/example2/create_servers_loops.tf
@@ -1,8 +1,11 @@
 resource "aws_instance" "frontend" { # here frontend is used for reference and is not the name
-  count = length(var.instances)
+  count = length(var.instances) 
   ami                    = "ami-03265a0778a880afb"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-017c17be83f3872d4"]
+  tags = {
+    Name =var.instances[count.index]
+  }
 }
 
 variable "instances" {
