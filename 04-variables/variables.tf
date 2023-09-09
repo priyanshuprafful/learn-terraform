c0365f04ec8178bacef6cb2d77c6011d246a1db2
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-09 17:09:48 +0530

    updating ./learn-terraform

--- 04-variables/variables.tf
+++ 04-variables/variables.tf
@@ -1,39 +1,56 @@
-variable "sample" {
-  default = "Hello World"
-}
-output "sample1" {
-  value = var.sample
+#variable "sample" {
+#  default = "Hello World"
+#}
+#output "sample1" {
+#  value = var.sample
+#}
+#
+#variable "sample2" {} # here we are not giving any value , or not declaring sample 2 ,
+#                      # actually it will fetch the sample 2 from terraform.tfvars file that we created
+#
+#output "sample3" {
+#  value = var.sample2
+#}
+#
+#variable "cli" {} # again empty variable as we are going to send it from cli
+#
+#output "cli1" {
+#  value = var.cli
+#}
+#
+#variable "input" {} # we are not even printing this
+#
+#variable "type1" {
+#  default = "Namaste" # this is normal one
+#}
+#variable "type2" {
+#  default = ["Namaste",1000,true,"World"] # this is list
+#}
+#variable "type3" { # this is map type
+#  default = {
+#    string = "Hello", # here string , number and boolean are just name or key given
+#    number = 100 ,
+#    boolean = true
+#  }
+#}
+#
+#output "mix_type" {
+#  value = "Variable Type1 is ${var.type1} , First value in list is ${var.type2[0]} , Boolean Value of Map is ${var.type3["boolean"]} , Number value of Map is ${var.type3["number"]} "
+#}
+
+variable "d1" {
+  default = [
+    {
+      course_name = "aws"
+      trainer_name = "Raju"
+    },
+    {
+      course_name = "devops"
+      trainer_name = "Ravi"
+    }
+  ]
 }
 
-variable "sample2" {} # here we are not giving any value , or not declaring sample 2 ,
-                      # actually it will fetch the sample 2 from terraform.tfvars file that we created
-
-output "sample3" {
-  value = var.sample2
-}
-
-variable "cli" {} # again empty variable as we are going to send it from cli
-
-output "cli1" {
-  value = var.cli
-}
-
-variable "input" {} # we are not even printing this
-
-variable "type1" {
-  default = "Namaste" # this is normal one
-}
-variable "type2" {
-  default = ["Namaste",1000,true,"World"] # this is list
-}
-variable "type3" { # this is map type
-  default = {
-    string = "Hello", # here string , number and boolean are just name or key given
-    number = 100 ,
-    boolean = true
-  }
-}
-
-output "mix_type" {
-  value = "Variable Type1 is ${var.type1} , First value in list is ${var.type2[0]} , Boolean Value of Map is ${var.type3["boolean"]} , Number value of Map is ${var.type3["number"]} "
+output "courses_name" {
+  value = var.d1.*.course_name
 }
