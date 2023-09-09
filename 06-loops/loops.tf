465edb71ad66c0fbf976378ff1eaa1cfb112a055
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-09 20:27:47 +0530

    updating ./learn-terraform

--- 06-loops/loops.tf
+++ 06-loops/loops.tf
@@ -3,5 +3,28 @@ variable "D2" {
 }
 
 resource "null_resource" "i_do_nothing" {
-  count = var.D2 # it will do nothing for 5 times 
+  count = var.D2 # it will do nothing for 5 times
+}
+variable "D3" {
+  default = ["apple" , "banana"]
+}
+resource "null_resource" "i_do_nothing_1" {
+  count = length(var.D3) # it will do nothing for 5 times but will take input from D3 list
+}                        # we use length as we have a list above
+
+variable "D4" {
+  default = {
+    pineapple = {
+      name = "Pineapple"
+      count = 3
+    }
+    mango = {
+      name = "Mango"
+      count = 30
+    }
+  }
+}
+
+resource "null_resource" "i_do_nothing_again" {
+  for_each = var.D4
 }
