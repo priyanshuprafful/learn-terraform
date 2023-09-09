89f2f015f7f98e9e1a99defe80510bf985df5552
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-09 21:03:34 +0530

    updating ./learn-terraform

---
+++ roboshop-setup/example3/servers-for_each-loop.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "instances" { # here instances is used for reference for terraform and is not the name
+  for_each = var.instances # will take the count and is used for referencing
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = each.value["type"] # here we want to pick type from below variable instances type
+  vpc_security_group_ids = ["sg-017c17be83f3872d4"]
+  tags = {
+    Name =each.value["name"]
+  }
+}
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name ="catalogue"
+      type = "t3.micro"
+    }
+    user = {
+      name ="user"
+      type = "t3.small"
+    }
+  }
+}
+
