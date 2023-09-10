1ff0cb0015ce6c0f6e9145e0233e94cea91cb99c
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-10 15:08:11 +0530

    updating ./learn-terraform

--- 07-conditions/conditions-example.tf
+++ 07-conditions/conditions-example.tf
@@ -1,5 +1,25 @@
+#resource "aws_instance" "test" {
+#  ami = "ami-03265a0778a880afb"
+#  instance_type = var.instance_type_1 # here since our variable below is empty it is going to pick up the
+#}                                     # default value which m1.small
+#variable "instance_type_1" {}
+#
+#variable "instances" {}
+#
+#resource "aws_instance" "test" {
+#  ami = "ami-03265a0778a880afb"
+#  instance_type = var.instances == "" ? "t3.micro" : var.instances
+#}
+# above if empty will create default value which is t3.micro , otherwise will create the one which
+# user has given.
+
+variable "instances" {}
+
 resource "aws_instance" "test" {
+  count = var.instances
   ami = "ami-03265a0778a880afb"
-  instance_type = var.instance_type_1 # here since our variable below is empty it is going to pick up the
-}                                     # default value which m1.small 
-variable "instance_type_1" {}
+  instance_type = var.instances == "" ? "t3.micro" : var.instances
+
+}
+
+
