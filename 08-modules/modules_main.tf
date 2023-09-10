108a6af87fe104b2bd94218700084cbf2178fe33
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-10 16:13:53 +0530

    updating ./learn-terraform

---
+++ 08-modules/modules_main.tf
@@ -0,0 +1,4 @@
+module "test" {
+  source = "./test" # ./ means current working directory and inside that we are having a test module
+                    # so here test is folder and inside that we have a file
+}
---
+++ 08-modules/test/test-main.tf
@@ -0,0 +1,1 @@
+resource "null_resource" "does_nothing" {}
