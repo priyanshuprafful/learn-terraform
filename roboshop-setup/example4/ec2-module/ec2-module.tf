6bce20c501bf0ffd4276a0970279efec59257896
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-11 19:41:28 +0530

    updating ./learn-terraform

--- roboshop-setup/example4/ec2-module/ec2-module.tf
+++ roboshop-setup/example4/ec2-module/ec2-module.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2"  {
   ami = data.aws_ami.ami.image_id
   instance_type = "${var.instance_type}" # we can give var.instance_type here also it is the same
-  vpc_security_group_ids = var.sg_from_ec2_variable_id
+  vpc_security_group_ids = [var.sg_from_ec2_variable_id]
   tags = {
     Name = var.component # here we are not using $ as we are not adding anything along with the component
   }
