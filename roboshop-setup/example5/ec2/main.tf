dbb88675575d5afbde7d65cd29f25dd031c24f80
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-12 18:19:11 +0530

    updating ./learn-terraform

---
+++ roboshop-setup/example5/ec2/main.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
+
+resource "aws_instance" "ec2"  {
+  ami = data.aws_ami.ami.image_id
+  instance_type = "${var.instance_type}" # we can give var.instance_type here also it is the same
+  vpc_security_group_ids = [var.sg_from_ec2_variable_id] # used from  module-server-creation-main.tf file
+  tags = {                                # there we have declared everything of sg-module.tf file
+    Name = var.component # here we are not using $ as we are not adding anything along with the component
+  }
+}
+
+
+variable "component" {}
+variable "instance_type" {}
+variable "sg_from_ec2_variable_id" {}
---
+++ roboshop-setup/example5/sg/main.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0 # 0 is for everyone and 22 is for ssh and 80 is for http like that
+    to_port          = 0
+    protocol         = "-1" # instead of tcp we give -1 which means allow all
+    cidr_blocks      = ["0.0.0.0/0"]
+    # so this becomes allow-all kind of a security group .
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+output "sg-output-id" {
+  value = aws_security_group.allow_tls.id # this is the value which we want to share or use in ec2-module
+}
