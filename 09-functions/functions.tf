43853cb6b23c78339630a125395e3b5feee24fd5
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-13 14:34:43 +0530

    updating ./learn-terraform

--- 09-functions/functions.tf
+++ 09-functions/functions.tf
@@ -1,15 +1,16 @@
 variable "test" {
   default = {
     "catalogue" = {
-      "private_ip" = "172.31.17.240"
+      "private_ipp" = "172.31.17.240"
     }
     "user" = {
-      "private_ip" = "172.31.26.244"
+      "private_ipp" = "172.31.26.244"
     }
   }
 }
 
 output "test-output" {
 #  value = var.test # to print whole value
-  value = var.test["catalogue"] # to print catalogue only 
+#  value = var.test["catalogue"] # to print catalogue only
+  value = var.test["catalogue"].private_ipp
 }
