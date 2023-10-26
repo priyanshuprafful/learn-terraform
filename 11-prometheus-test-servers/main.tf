1283f5362a9a07795339d53fdbc2a73c347b9388
priyanshuprafful <priyanshuprafful@gmail.com>
2023-10-26 19:28:19 +0530

    updating ./learn-terraform

--- 11-prometheus-test-servers/main.tf
+++ 11-prometheus-test-servers/main.tf
@@ -12,10 +12,11 @@ resource "aws_spot_instance_request" "prometheus_node_1" {
 resource "aws_spot_instance_request" "prometheus_node_2" {
   ami = "ami-0ce9a5bb4362d4c85"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-017c17be83f3872d4"]
+  wait_for_fulfillment = true
 
   tags = {
     Name = "prom-test-server2"
-    vpc_security_group_ids = ["sg-017c17be83f3872d4"]
-    wait_for_fulfillment = true
+
   }
 }
